fcc8062a9009fb01379b79c405061bac7d90edd1
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-06-21 04:43:10 -0400

    check employee's present

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -5,7 +5,13 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
 		// TODO Auto-generated method stub
-
+		int employeePresent = 1;
+		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
+		if (isPresent == employeePresent)
+			System.out.println("Empolyee is Present");
+		else
+			System.out.println("Employee is Absent");
+			
 	}
 
 }
