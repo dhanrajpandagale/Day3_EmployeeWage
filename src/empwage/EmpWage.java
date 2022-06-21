da7e565dd61541afa15fe73df0451b283f58e3fb
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-06-21 05:01:34 -0400

    Calculating employee's daily wage

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -6,12 +6,16 @@ public class EmpWage {
 		System.out.println("Welcome to employee wage");
 		// TODO Auto-generated method stub
 		int employeePresent = 1;
+		int wagePerHour = 20;
+		int numberOfHours = 8;
 		int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
-		if (isPresent == employeePresent)
+		if (isPresent == employeePresent) {
 			System.out.println("Empolyee is Present");
-		else
+			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+		} else {
 			System.out.println("Employee is Absent");
-			
+			System.out.println("Employee earn = 0");
+		}
 	}
 
 }
