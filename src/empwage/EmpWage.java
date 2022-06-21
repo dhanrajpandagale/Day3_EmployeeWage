db1a4f94137c5cfdabdde335000bb7cbd98679ed
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-06-21 04:21:53 -0400

    Welcome Message

---
+++ src/empwage/EmpWage.java
@@ -0,0 +1,11 @@
+package empwage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage");
+		// TODO Auto-generated method stub
+
+	}
+
+}
